19aff950780041c494354fcaf00bcd2da853e846
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2025-01-12 14:31:49 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-4xl bg-red-400 text-accent text-center p-5 mt-10 rounded-full font-bold">Hello wrold</h1>
+    </div>
+  );
+};
+
+export default App;
